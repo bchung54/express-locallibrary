147767b22055b621adf60ffdc78341c8607d8e5a
bchung-desktop <brianchung54@gmail.com>
2023-05-29 10:27:54 -0700

    Add files and changes required for deployment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -14,8 +15,7 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const mongoDB =
-  'mongodb+srv://admin:sHTAPOal6SVhlLVc@cluster0.522ciwl.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URL;
 
 main().catch((err) => console.log(err));
 async function main() {
